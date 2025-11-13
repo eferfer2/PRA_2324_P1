2ece9ef84260747247cc40ed13d193c416d887af
EFERFER2 <eferfer2@alumno.upv.es@ldsic-vdi10.upvnet.upv.es>
2025-11-13 12:30:36 +0100

    Añadida implementación de la clase Node

---
+++ node.h
@@ -0,0 +1,22 @@
+#include <ostream>
+#ifndef NODE_H
+#define NODE_H
+template <typename T>
+class Node {
+    public:
+
+        T data;
+        Node<T>* next;
+
+        // Constructor
+        Node(T data, Node<T>* next = nullptr)
+            : data(data), next(next) {}
+
+        // Sobrecarga del operador <<
+        friend std::ostream& operator<<(std::ostream &out, const Node<T> &node) {
+            out << node.data;
+            return out;
+        }
+};
+
+#endif
