ef80dcf7dafa1d4896a00316bcf79cf622ce9f02
EFERFER2 <eferfer2@alumno.upv.es@ldsic-vdi10.upvnet.upv.es>
2025-11-13 11:56:16 +0100

    Añadida interfaz genérica List

---
+++ list.h
@@ -0,0 +1,26 @@
+#ifndef LIST_H
+#define LIST_H
+#include <stdexcept>
+
+template <typename T>
+class List{
+
+    public:
+    void insert(int pos, T& e);
+    void append(T& e);
+    void prepend(T& e);
+    T remove(int pos);
+    T get(int pos);
+    int search(T& e);
+    bool empty();
+    int size();
+
+
+
+
+
+};
+
+
+
+#endif
